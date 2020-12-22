4563a72f46fb137df90503c21229e430f1dc4e3f
Denis Kozyrev <dzianis.kozyrau@itechart-group.com>
2020-12-22 17:58:59 +0300

    Implemented gatsby contentful netlify project example

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
